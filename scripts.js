b037adff427ff010ce31c611af802029497eaeef
Alina <changatasan@gmail.com>
2024-10-17 23:03:23 +0400

    trying fix js copying functions

--- scripts.js
+++ scripts.js
@@ -1,20 +1,22 @@
+function ctc() {
+    navigator.clipboard.writeText("catsoulisme@gmail.com");
+};
+function ctc1() {
+    navigator.clipboard.writeText("@CatSoulIsMe");
+};
+function ctc2() {
+    navigator.clipboard.writeText("+79090566540");
+};
+function show (bb) {
+    bb.style.display = 'block';
+};
+function hide (bb) {
+    bb.style.display = 'none';
+};
+    
     
     window.onload = function(){
-        function ctc(hh) {
-            navigator.clipboard.writeText("catsoulisme@gmail.com");
-        };
-        function ctc1(hh) {
-            navigator.clipboard.writeText("@CatSoulIsMe");
-        };
-        function ctc2(hh) {
-            navigator.clipboard.writeText("+79090566540");
-        };
-        function show (bb) {
-            bb.style.display = 'block';
-        };
-        function hide (bb) {
-            bb.style.display = 'none';
-        };
+        
         document.getElementById("language-toggle").onclick = function () {
             if ($(this).is(":checked")) {
                 for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
@@ -29,4 +31,5 @@
                 }
             }
         };
+        
     }
