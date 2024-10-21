fdceac77016bde20bdeb82d5c1c8627ccdbaf26f
Alina <changatasan@gmail.com>
2024-10-21 22:46:15 +0400

    added language localstorage to save checkbox between navigations

--- scripts.js
+++ scripts.js
@@ -14,22 +14,45 @@ function hide (bb) {
     bb.style.display = 'none';
 };
     
-    
-    window.onload = function(){
-        
-        document.getElementById("language-toggle").onclick = function () {
-            if ($(this).is(":checked")) {
-                for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
-                    show(document.getElementsByClassName("ru")[i]);
-                    hide(document.getElementsByClassName("en")[i]);
-                }
+const localLang = window.localStorage.getItem('lang')
 
-            } else {
-                for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
-                    show(document.getElementsByClassName("en")[i]);
-                    hide(document.getElementsByClassName("ru")[i]);
-                }
-            }
-        };
-        
+
+
+
+window.onload = function(){ 
+            
+    if (localLang == "ru") {
+        for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
+            show(document.getElementsByClassName("ru")[i]);
+            hide(document.getElementsByClassName("en")[i]);
+            document.getElementById("language-toggle").checked = true;
+        }
+    } else {
+        for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
+            show(document.getElementsByClassName("en")[i]);
+            hide(document.getElementsByClassName("ru")[i]);
+            document.getElementById("language-toggle").checked = false;
+        }
     }
+        
+    document.getElementById("language-toggle").onclick = function () {
+        if ($(this).is(":checked") || localLang != "ru") {
+            for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
+                show(document.getElementsByClassName("ru")[i]);
+                hide(document.getElementsByClassName("en")[i]);
+                    
+            }
+            window.localStorage.setItem('lang', 'ru')
+            
+                
+        } else {
+            for(var i=0; i < document.getElementsByClassName("en").length; i++) { 
+                show(document.getElementsByClassName("en")[i]);
+                hide(document.getElementsByClassName("ru")[i]);
+                    
+            }
+            window.localStorage.setItem('lang', 'eng')
+        }
+    };
+        
+}
